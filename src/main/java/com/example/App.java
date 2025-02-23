1c4e24372a43ed1779abde7755ca46371cdc846c
McKenna <mckenna.j.pasquale@gmail.com>
2025-02-23 16:23:49 -0500

    Updated project structure, fixed naming issues, and added Dockerfile

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,8 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Midterm Project!");
+        System.out.println("Hello, this is my Midterm Project!");
+        System.out.println("I had several rounds of troubleshooting and failed deployments but am happy to announce it is now successful");
+        System.out.println("My favorite candy is white chocolate kit-kats (:");
     }
 }
---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,12 @@
+
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+    @Test
+    void testMainOutput() {
+        assertEquals(2, 1 + 1); // Simple test to demonstrate JUnit
+    }
+}
